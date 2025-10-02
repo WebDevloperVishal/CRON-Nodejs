0d7e95018dfc5219771d9108477f8eda9aade3b4
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-03 01:00:05 +0530

    commit

--- server.js
+++ server.js
@@ -4,14 +4,35 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
+
 const port = 3000
 
 app.use(express.json())
 
+const sourceDir = path.join(__dirname, "data");
+const backupDir = path.join(__dirname, "backups");
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-cron.schedule('* * * * *', () => {
-    console.log('Running a task in each and every task');
+cron.schedule('* * * * *', async () => {
+    try {
+        const timestamp = new Date().toISOString().replace(/[:.]/g, '-')
+        const destination = path.join(backupDir, `backup-${timestamp}`) 
+
+
+        await fs.cp(sourceDir, destination, { recursive: true},( err)=>{
+            if (err) {
+                console.log('Backup failed :',err);
+            } else {
+                console.log(`Bacckup created at ${destination}`);   
+            }
+        })
+
+        
+    } catch (error) {
+        console.log('Backup failed :', err);
+        
+    }
     
 })
 
